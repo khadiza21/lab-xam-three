984eaabc1d72ec9cc02ebbace7515fe2c5edeae4
khadiza21 <bibikhadiza474@gmail.com>
2022-04-29 20:37:36 +0600

    lab xam three wite c

--- withC/D-D.c
+++ withC/D-D.c
@@ -2,10 +2,36 @@
 int main()
 {
 
-   // scanf("%d %d %d", &leavingTime, &enteringTime, &checkingTime);
+    int number, anton = 0, danik = 0;
+    char word[100000];
+    scanf("%d", &number);
+    scanf("%s", word);
 
-// left 
-    
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+
+        if (word[i] == 65)
+        {
+            anton++;
+        }
+        else if (word[i] == 68)
+        {
+            danik++;
+        }
+    }
+
+    if (anton > danik)
+    {
+        printf("Anton\n");
+    }
+    else if (anton < danik)
+    {
+        printf("Danik\n");
+    }
+    else if (anton == danik)
+    {
+        printf("Friendship\n");
+    }
 
     return 0;
 }
